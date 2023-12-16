5a816a8d14620523bd812b125aa8e38a7029809b
hugosenari <hugosenari@gmail.com>
2023-12-16 01:46:25 -0300

    feat: run flake with args

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
   {
-    packages.x86_64-linux.genix7000 = pkgs.writeScriptBin "genix7000" "${pkgs.openscad}/bin/openscad ${./genix.scad}";
+    packages.x86_64-linux.genix7000 = pkgs.writeScriptBin "genix7000" "${pkgs.openscad}/bin/openscad $@ ${./genix.scad}";
     packages.x86_64-linux.default = self.packages.x86_64-linux.genix7000;
   };
 }
